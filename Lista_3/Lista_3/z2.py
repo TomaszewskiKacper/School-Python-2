3058fc3e9779e7ec05eab39b80ce8732e4d96042
TomaszewskiKacper <94453117+TomaszewskiKacper@users.noreply.github.com>
2024-03-22 09:59:56 +0100

    3 finished

--- Lista_3/Lista_3/z2.py
+++ Lista_3/Lista_3/z2.py
@@ -20,11 +20,58 @@ class Ulamek:
         self.a //= div
         self.b //= div
 
+    @staticmethod
+    def dodaj(x, y):
+        a = (x.a * y.b) + (y.a * x.b)
+        b = x.b * y.b
+        return Ulamek(a,b)
+
+    @staticmethod
+    def odejmij(x, y):
+        a = (x.a * y.b) - (y.a * x.b)
+        b = x.b * y.b
+        return Ulamek(a,b)
+
+    @staticmethod
+    def mnoz(x, y):
+        a = x.a * y.a
+        b = x.b * y.b
+        return Ulamek(a,b)
+
+    @staticmethod
+    def dziel(x, y):
+        a = x.a * y.b
+        b = x.b * y.a
+        return Ulamek(a,b)
+
+
     
 
 
-u = Ulamek(10,4)
-u.wypisz()
-u.skroc()
-u.wypisz()
+u1 = Ulamek(10,4)
+u2 = Ulamek(5,3)
+
+
+#testowanie
+u1.wypisz()
+u1.skroc()
+print("skrocone: ")
+u1.wypisz()
+print("")
+u1.wypisz()
+u2.wypisz()
+print("dodane: ")
+u3 = Ulamek.dodaj(u1, u2)
+u3.wypisz()
+print("odejmowane: ")
+u3 = Ulamek.odejmij(u1,u2)
+u3.wypisz()
+print("mnozone: ")
+u3 = Ulamek.mnoz(u1,u2)
+u3.wypisz()
+print("dzielone: ")
+u3 = Ulamek.dziel(u1,u2)
+u3.wypisz()
+
+
  
---
+++ Lista_3/Lista_3/z3.py
@@ -0,0 +1,48 @@
+﻿class Student:
+    def __init__(self, Imie, Nazwisko, Index): #constructor
+        self.imie = Imie      
+        self.nazwisko = Nazwisko
+        self.index = Index
+        self.przedmioty = dict()
+        self.ocena = 0
+
+    def Update(self):
+        if len(self.przedmioty) != 0:
+            o = 0
+            for p in self.przedmioty:
+                o+= self.przedmioty[p]
+            o /= len(self.przedmioty)
+            self.ocena = o     
+        else:
+            self.ocena = 0
+
+       
+
+    def Dodaj_przedmiot(self, przedmiot, ocena):
+        self.przedmioty[przedmiot] = ocena
+        self.Update()
+
+    def Usun_przedmiot(self, przedmiot):
+        del self.przedmioty[przedmiot]
+        self.Update()
+
+
+s = Student("Kac", "Tom", 33)
+s.Dodaj_przedmiot("a", 5)
+s.Dodaj_przedmiot("b", 4)
+s.Dodaj_przedmiot("c", 3)
+s.Dodaj_przedmiot("d", 2)
+print(s.ocena)
+s.Usun_przedmiot("a")
+s.Usun_przedmiot("b")
+s.Usun_przedmiot("c")
+s.Usun_przedmiot("d")
+print(s.ocena)
+    
+
+    
+
+
+
+
+
